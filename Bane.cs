4102fe4775962a74502829089d2cb7654d3475ca
guchitarou <zhinengr@gmail.com>
2020-01-25 17:25:14 +0900

    program

---
+++ Bane.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Bane : MonoBehaviour
+{
+    public GameObject target,anchor;
+    float k=50,S=6;
+    Vector3 dee,dee2,f;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = target.GetComponent<Rigidbody2D>();  // rigidbodyを取得
+    }
+    void Update()
+    {
+        dee = (anchor.transform.position - target.transform.position)*k;
+        dee2 = (dee - (f - dee)*S);// 力を設定
+        f = dee;
+        Vector3 force = dee2;    
+        rb.AddForce(force);  // 力を加える
+    }
+}
